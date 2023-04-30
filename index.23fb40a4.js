4a85b77cc4e3706bff1d14c6ab58547bd47af46b
KlymentKim <49413026+KlymentKim@users.noreply.github.com>
2023-04-30 16:39:22 +0000

    Deploying to gh-pages from @ KlymentKim/goit-js-hw-11@b21ff6463759651f7ffd7b559a5f4f2ef1455c73 🚀

---
+++ index.23fb40a4.js
@@ -0,0 +1,2 @@
+document.getElementById("search-form"),document.querySelector(".search-form input"),document.querySelector(".search-form button");notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+//# sourceMappingURL=index.23fb40a4.js.map
---
+++ index.ca2f4401.js
@@ -0,0 +1,2 @@
+!function(){document.getElementById("search-form"),document.querySelector(".search-form input"),document.querySelector(".search-form button");notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")}();
+//# sourceMappingURL=index.ca2f4401.js.map
