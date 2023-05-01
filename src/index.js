c16d5dde43529e2d13ec6606334dc524f275296a
Klyment <klim86kr@gmail.com>
2023-05-01 16:51:23 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-// import notiflix;
-
-const form = document.getElementById('search-form');
-const input = document.querySelector('.search-form input');
-const button = document.querySelector('.search-form button');
-
-const url = 'https://pixabay.com/api/?key={YOUR_API_KEY}&q={SEARCH_TERM}&image_type=photo&orientation=horizontal&safesearch=true'
-
-notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+import './sass/main.scss';
+import './js/search-images/index';
