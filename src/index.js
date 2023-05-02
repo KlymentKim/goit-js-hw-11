a7462093d21c4970762b3013a0d543aa0973f8a8
Klyment <klim86kr@gmail.com>
2023-05-02 18:24:48 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-// import notiflix;
+import notiflix from 'notiflix';
 
-const form = document.getElementById('search-form');
-const input = document.querySelector('.search-form input');
-const button = document.querySelector('.search-form button');
+// const form = document.getElementById('search-form');
+// const input = document.querySelector('.search-form input');
+// const button = document.querySelector('.search-form button');
 
-const url = 'https://pixabay.com/api/?key={YOUR_API_KEY}&q={SEARCH_TERM}&image_type=photo&orientation=horizontal&safesearch=true'
+// const url = 'https://pixabay.com/api/?key={YOUR_API_KEY}&q={SEARCH_TERM}&image_type=photo&orientation=horizontal&safesearch=true'
 
-notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+// notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
