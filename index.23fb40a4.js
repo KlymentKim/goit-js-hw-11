6d71018354d3cdc28b5788361331d7ae3a5aab0e
KlymentKim <49413026+KlymentKim@users.noreply.github.com>
2023-05-02 15:45:40 +0000

    Deploying to gh-pages from @ KlymentKim/goit-js-hw-11@ea842cf53dae311b2647f18efd033237de11eea4 🚀

--- index.23fb40a4.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("search-form"),document.querySelector(".search-form input"),document.querySelector(".search-form button");notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-//# sourceMappingURL=index.23fb40a4.js.map
--- index.ca2f4401.js
+++
@@ -1,2 +0,0 @@
-!function(){document.getElementById("search-form"),document.querySelector(".search-form input"),document.querySelector(".search-form button");notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")}();
-//# sourceMappingURL=index.ca2f4401.js.map
